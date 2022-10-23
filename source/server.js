3ea6774effb01eec791d7169841de1098eb0f685
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-23 00:08:36 -0300

    Rota importada.

--- source/server.js
+++ source/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const router = require("./routes/books");
 const server = express();
 
 const PORT = process.env.PORT || 3000;
 
-server.use("/", express.json());
+server.use("/", [router, express.json()]);
 
 server.listen(PORT, () => console.log("Server On."));
