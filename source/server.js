295beb4e6aaede68ba49d7105714a7182bf1159e
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-01 10:17:47 -0300

    fix: Alterado a ordem na lógica do server.use()

--- source/server.js
+++ source/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = require("./routes/books");
-const server = express();
 
+const server = express();
 const PORT = process.env.PORT || 3000;
 
-server.use("/", [router, express.json()]);
+server.use(express.json(), router);
 
 server.listen(PORT, () => console.log("Server On."));
