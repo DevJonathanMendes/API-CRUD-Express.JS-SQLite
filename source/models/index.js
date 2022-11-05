68083ba5e27ed84298810c3bf1a7b57492a4df5b
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-05 17:19:03 -0300

    feat: Adicionado 4 livros pra base

--- source/models/index.js
+++ source/models/index.js
@@ -3,16 +3,23 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./source/database/books.db", err => {
     if (err) throw new Error(err.message);
 });
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS books");
+    db.run(
+        `CREATE TABLE IF NOT EXISTS books
+        (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL UNIQUE,
+            author TEXT NOT NULL,
+            pages INTEGER NOT NULL,
+            published INTEGER NOT NULL
+        );`);
 
-db.run(
-    `CREATE TABLE IF NOT EXISTS books
-    (
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL UNIQUE,
-        author TEXT NOT NULL,
-        pages INTEGER NOT NULL,
-        published INTEGER NOT NULL
-    );`
-);
+    const insert = "INSERT INTO books (title, author, pages, published) VALUES (?, ?, ?, ?)"
+    db.run(insert, ['c++: como programar', 'deitel', 1208, 2006]);
+    db.run(insert, ['código limpo', 'robert c. martin', 425, 2009]);
+    db.run(insert, ['javascript: o guia definitivo', 'david flanagan', 1080, 2012]);
+    db.run(insert, ['padrões javascript', 'david flanagan', 340, 2010]);
+});
 
 module.exports = db;
