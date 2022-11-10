20cfa7687a5ae80ef1537be4f66f2d335f4eec2d
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-10 18:59:09 -0300

    fix: Removido comando SQL DROP TABLE

--- source/models/index.js
+++ source/models/index.js
@@ -3,8 +3,8 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./source/database/books.db", err => {
     if (err) throw new Error(err.message);
 });
+
 db.serialize(() => {
-    db.run("DROP TABLE IF EXISTS books");
     db.run(
         `CREATE TABLE IF NOT EXISTS books
         (
