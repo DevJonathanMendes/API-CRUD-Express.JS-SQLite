e570587b465a57ff5518df43d36e5054218c0507
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-11 15:05:38 -0300

    refactor: func startDatabase

--- source/models/index.js
+++ source/models/index.js
@@ -4,22 +4,24 @@ const db = new sqlite3.Database("./source/database/books.db", err => {
     if (err) throw new Error(err.message);
 });
 
-db.serialize(() => {
-    db.run(
-        `CREATE TABLE books
-        (
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL UNIQUE,
-            author TEXT NOT NULL,
-            pages INTEGER NOT NULL,
-            published INTEGER NOT NULL
-        );`);
+const startDatabase = () => {
+    db.serialize(() => {
+        db.run(`CREATE TABLE books
+            (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL UNIQUE,
+                author TEXT NOT NULL,
+                pages INTEGER NOT NULL,
+                published INTEGER NOT NULL
+            );`
+        );
 
-    const insert = "INSERT INTO books (title, author, pages, published) VALUES (?, ?, ?, ?)"
-    db.run(insert, ['c++: como programar', 'deitel', 1208, 2006]);
-    db.run(insert, ['código limpo', 'robert c. martin', 425, 2009]);
-    db.run(insert, ['javascript: o guia definitivo', 'david flanagan', 1080, 2012]);
-    db.run(insert, ['padrões javascript', 'david flanagan', 340, 2010]);
-});
+        const insert = "INSERT INTO books (title, author, pages, published) VALUES (?, ?, ?, ?)"
+        db.run(insert, ['c++: como programar', 'deitel', 1208, 2006]);
+        db.run(insert, ['código limpo', 'robert c. martin', 425, 2009]);
+        db.run(insert, ['javascript: o guia definitivo', 'david flanagan', 1080, 2012]);
+        db.run(insert, ['padrões javascript', 'david flanagan', 340, 2010]);
+    });
+};
 
-module.exports = db;
+module.exports = { db, startDatabase };
