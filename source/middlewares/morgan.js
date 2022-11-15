4a427d4c8eaebf1c3823ab832ea3e0b5e3f50343
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-15 17:22:45 -0300

    feat: Criado middleware para log HTTP

---
+++ source/middlewares/morgan.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const morgan = require("morgan");
+const logger = require("../utils/logger");
+
+const stream = { write: message => logger.http(message) };
+
+const skip = () => {
+    const env = process.env.NODE_ENV || "development";
+    return env !== "development";
+};
+
+const morganMiddleware = morgan(":remote-addr :method :url :status :res[content-length] - :response-time ms", { stream, skip });
+
+module.exports = morganMiddleware;
