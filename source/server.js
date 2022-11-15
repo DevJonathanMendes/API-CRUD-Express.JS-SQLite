b4613ea72943027239317a8d38035e514f341410
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-15 17:30:45 -0300

    feat: Importado logger e morganMiddleware

--- source/server.js
+++ source/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { startDatabase } = require("./models/index");
 const router = require("./routes/books");
+const logger = require("./utils/logger");
+const morganMiddleware = require("./middlewares/morgan");
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 startDatabase();
+server.use(morganMiddleware);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(router);
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.listen(PORT, () => logger.debug(`Server listening on port ${PORT}`));
