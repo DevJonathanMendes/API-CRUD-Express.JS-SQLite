ac79ca9144e7a853944e7e87b5f6e4f37cf34406
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-17 07:40:14 -0300

    feat: Importado helmet

--- source/app.js
+++ source/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { startDatabase } = require("./models/index");
 const morganMiddleware = require("./middlewares/morgan");
+const helmet = require("helmet");
 const router = require("./routes/books");
 
 const app = express();
 
 startDatabase();
 app.use(morganMiddleware);
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
