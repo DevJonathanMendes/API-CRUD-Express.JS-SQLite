94775d75334ce7dda780eac24474ca4569078f7a
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-17 07:50:16 -0300

    refactor: Removido func middleware de CORS

--- source/routes/books.js
+++ source/routes/books.js
@@ -3,17 +3,6 @@ const controllers = require("../controllers/index");
 
 const router = express.Router();
 
-/* // CORS.
-router.use((req, res, next) => {
-    res.header({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE",
-        "Access-Control-Allow-Headers": " Content-Type, Origin, X-Requested-With, Accept",
-        "Access-Control-Allow-Credentials": "true"
-    })
-    next();
-}); */
-
 router.route("/books")
     .get(controllers.getAllBooks)
     .post(controllers.createBook);
