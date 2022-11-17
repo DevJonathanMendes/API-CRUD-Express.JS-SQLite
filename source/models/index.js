ac615af52b9bd338afd14b20dc8e6dbc8e9c2ec3
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-17 09:19:09 -0300

    refactor: Evita errors

--- source/models/index.js
+++ source/models/index.js
@@ -1,7 +1,13 @@
 const sqlite3 = require("sqlite3").verbose();
+const { existsSync, mkdirSync } = require("fs");
+const { debug } = require("../utils/logger");
+
+existsSync("source/database")
+    ? debug("Pasta já existe.")
+    : mkdirSync("source/database");
 
 const db = new sqlite3.Database("./source/database/books.db", err => {
-    if (err) throw new Error(err.message);
+    if (err) debug(err.message);
 });
 
 const startDatabase = () => {
@@ -13,14 +19,24 @@ const startDatabase = () => {
                 author TEXT NOT NULL,
                 pages INTEGER NOT NULL,
                 published INTEGER NOT NULL
-            );`
+            );`,
+            err => err ? debug("Tabela já existe.")
+                : debug("Tabela criada.")
         );
 
-        const insert = "INSERT INTO books (title, author, pages, published) VALUES (?, ?, ?, ?)"
-        db.run(insert, ['c++: como programar', 'deitel', 1208, 2006]);
-        db.run(insert, ['código limpo', 'robert c. martin', 425, 2009]);
-        db.run(insert, ['javascript: o guia definitivo', 'david flanagan', 1080, 2012]);
-        db.run(insert, ['padrões javascript', 'david flanagan', 340, 2010]);
+        const insert = "INSERT INTO books (title, author, pages, published) VALUES (?, ?, ?, ?)";
+        const books = [
+            ['c++: como programar', 'deitel', 1208, 2006],
+            ['código limpo', 'robert c. martin', 425, 2009],
+            ['javascript: o guia definitivo', 'david flanagan', 1080, 2012],
+            ['padrões javascript', 'david flanagan', 340, 2010]
+        ];
+
+        books.forEach((book) =>
+            db.run(insert, book, err =>
+                err ? debug(`Livro já existe: ${book[0]}`)
+                    : debug(`Livro criado: ${book[0]}`))
+        );
     });
 };
 
