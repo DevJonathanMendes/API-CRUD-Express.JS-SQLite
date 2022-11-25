97ba0091b6166b9a424f157c5f5ce64089ce2759
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-25 17:23:48 -0300

    refactor: Removido Winston-log do Morgan-log

--- source/middlewares/morgan.js
+++ source/middlewares/morgan.js
@@ -1,14 +1,11 @@
 require("dotenv").config();
 const morgan = require("morgan");
-const logger = require("../utils/logger");
-
-const stream = { write: message => logger.http(message) };
 
 const skip = () => {
     const env = process.env.NODE_ENV || "development";
     return env !== "development";
 };
 
-const morganMiddleware = morgan(":remote-addr :method :url :status :res[content-length] - :response-time ms", { stream, skip });
+const morganMiddleware = morgan("dev", { skip });
 
 module.exports = morganMiddleware;
