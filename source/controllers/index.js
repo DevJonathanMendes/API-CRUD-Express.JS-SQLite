7cdac2683028de6b59a270c5aa2a51101ddb91a1
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-11-27 18:52:21 -0300

    refactor: Método patchBook

--- source/controllers/index.js
+++ source/controllers/index.js
@@ -1,49 +1,11 @@
 const sql = require("../models/index");
+const { validateJSON, getId } = require("../utils/utils");
 const log = require("../utils/logger");
-
-const throwError = message => {
-    throw new Error(message);
-};
-
-const getId = id => /^\d*$/.test(id) ? id : null;
-const validateValues = book => {
-    return new Promise((resolve, reject) => {
-        try {
-            Object.keys(book).forEach(item => {
-                switch (item) {
-                    case "id":
-                        throwError("You cannot set the ID.");
-                        break;
-                    case "title":
-                        if (!/^[\d\D]{2,64}$/.test(book[item])) {
-                            throwError("Invalid Title.");
-                        }; break;
-                    case "author":
-                        if (!/^[\d\D]{2,64}$/.test(book[item])) {
-                            throwError("Invalid Author.");
-                        }; break;
-                    case "pages":
-                        if (!/^\d{1,5}$/.test(book[item])) {
-                            throwError("Invalid Pages.");
-                        }; break;
-                    case "published":
-                        if (!/^\d{4}$/.test(book[item])) {
-                            throwError("Invalid Published.");
-                        }; break;
-                    default: throwError("Invalid JSON.");
-                };
-            });
-            resolve();
-        } catch (err) {
-            reject(err.message);
-        };
-    });
-};
 
 const controllers = {
     createBook: async (req, res) => {
         Promise.all([
-            validateValues(req.body),
+            validateJSON(req.body),
             new Promise((resolve, reject) => {
                 const { title, author, pages, published } = req.body;
                 const columns = "title, author, pages, published";
@@ -80,19 +42,24 @@ const controllers = {
         });
     },
 
-    patchBook: async (req, res) => {
-        Promise.all([
-            validateValues(req.body),
-            new Promise((resolve, reject) => {
-                const id = getId(req.params.id) || reject();
-                const set = Object.entries(req.body).map(([key, value]) =>
-                    `${key}='${typeof value == "string" ? value.toLowerCase() : value}'`);
-                const update = `UPDATE books SET ${set} WHERE id=${id}`;
+    patchBook: (req, res) => {
+        try {
+            validateJSON(req.body);
+            const id = getId(req.params.id);
+            const set = Object.entries(req.body).map(([key, value]) =>
+                `${key}='${typeof value == "string" ? value.toLowerCase() : value}'`);
+            const update = `UPDATE books SET ${set} WHERE id=${id}`;
 
-                db.run(update, err => err ? reject("Title Already Exists.") : resolve());
-            })])
-            .then(() => res.status(200).send("Ok"))
-            .catch(err => res.status(400).send(err.message));
+            sql.run(update, err => {
+                if (err) {
+                    res.status(500).send("Internal Server Error.");
+                    return log.error(err.message);
+                };
+                res.status(200).send("Ok.");
+            });
+        } catch (err) {
+            res.status(400).send(err.message);
+        };
     },
 
     deleteBook: (req, res) => {
