97803af706fd8d9f913bfa3a482d0fa0db8a734d
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-12-01 14:00:12 -0300

    feat: Adicionado res para rota não encontrada

--- source/routes/index.js
+++ source/routes/index.js
@@ -4,5 +4,8 @@ const booksRouter = require("./booksRouter");
 const router = express.Router();
 
 router.use("/books", booksRouter);
+router.use((req, res, next) => {
+    res.status(404).send("Not Found.");
+});
 
 module.exports = router;
