769cb0317741b894521d7e9531719b53163874b2
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-01-17 11:02:39 -0300

    refactor: Removido winston logger

--- source/server.js
+++ source/server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = require("./app");
-const logger = require("./utils/logger");
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.use(app);
 
-server.listen(PORT, () => logger.debug(`Server listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
