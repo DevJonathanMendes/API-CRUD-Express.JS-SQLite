ebc733b086daffe20b5f7e552686818ce7420d96
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-01-23 16:10:28 -0300

    test: Created - createBook route

--- tests/controllers.test.js
+++ tests/controllers.test.js
@@ -3,10 +3,9 @@ const app = require("../source/app");
 
 const { any, arrayContaining } = expect;
 
-describe("Testing the getAllBooks route", () => {
+describe("Testing the getAllBooks method that gets (if any) all books", () => {
     it("Should return a success answer with or without all books", async () => {
         const res = await request(app).get("/books");
-
         const book = expect.objectContaining({
             id: any(Number),
             title: any(String),
@@ -21,6 +20,26 @@ describe("Testing the getAllBooks route", () => {
             status: "Ok",
             books: res.body.books.length > 0 ?
                 arrayContaining([book]) : arrayContaining([])
+        });
+    });
+});
+
+describe("Testing the createBook method that creates a book", () => {
+    it("It should be possible to create a book", async () => {
+        const randomNum = () => Math.round(Math.random() * 1000)
+        const book = {
+            title: `Book ${randomNum()}`,
+            author: `Author ${randomNum()}`,
+            pages: randomNum(),
+            published: 1995
+        };
+        const res = await request(app).post("/books").send(book);
+
+        expect(res.status).toBe(201);
+        expect(res.body).toMatchObject({
+            response: true,
+            status: "Created",
+            book: book
         });
     });
 });
