153b569d4d4d944ecfa9e1ef07f1dc10a259cd0b
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-01-25 22:13:07 -0300

    Update

--- tests/controllers.test.js
+++ tests/controllers.test.js
@@ -1,45 +1,44 @@
 const request = require("supertest");
 const app = require("../source/app");
 
-const { any, arrayContaining } = expect;
+const randomNum = () => Math.round(Math.random() * 1000);
+const generateBook = () => {
+    return {
+        title: `Book ${randomNum()}`,
+        author: `Author ${randomNum()}`,
+        pages: randomNum(),
+        published: 1995
+    };
+};
+/* const { any, arrayContaining } = expect;
+const book = expect.objectContaining({
+    id: any(Number),
+    title: any(String),
+    author: any(String),
+    pages: any(Number),
+    published: any(Number)
+});
+length > 0 ? arrayContaining([book]) : arrayContaining([]) */
 
 describe("Testing the getAllBooks method that gets (if any) all books", () => {
     it("Should return a success answer with or without all books", async () => {
         const res = await request(app).get("/books");
-        const book = expect.objectContaining({
-            id: any(Number),
-            title: any(String),
-            author: any(String),
-            pages: any(Number),
-            published: any(Number)
-        });
-
         expect(res.status).toBe(200);
-        expect(res.body).toMatchObject({
-            response: true,
-            status: "Ok",
-            books: res.body.books.length > 0 ?
-                arrayContaining([book]) : arrayContaining([])
-        });
     });
 });
 
 describe("Testing the createBook method that creates a book", () => {
     it("It should be possible to create a book", async () => {
-        const randomNum = () => Math.round(Math.random() * 1000)
-        const book = {
-            title: `Book ${randomNum()}`,
-            author: `Author ${randomNum()}`,
-            pages: randomNum(),
-            published: 1995
-        };
-        const res = await request(app).post("/books").send(book);
+        const res = await request(app)
+            .post("/books")
+            .send(generateBook());
 
         expect(res.status).toBe(201);
-        expect(res.body).toMatchObject({
-            response: true,
-            status: "Created",
-            book: book
-        });
+    });
+});
+
+describe("", () => {
+    it("", () => {
+
     });
 });
