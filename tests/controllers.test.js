558c4da6452694a78496593c910f0210e8781c35
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-01-27 20:17:59 -0300

    refactor: tests

--- tests/controllers.test.js
+++ tests/controllers.test.js
@@ -2,14 +2,13 @@ const request = require("supertest");
 const app = require("../source/app");
 
 const randomNum = () => Math.round(Math.random() * 1000);
-const generateBook = () => {
-    return {
-        title: `Book ${randomNum()}`,
-        author: `Author ${randomNum()}`,
-        pages: randomNum(),
-        published: 1995
-    };
+const book = {
+    title: `Book ${randomNum()}`,
+    author: `Author ${randomNum()}`,
+    pages: randomNum(),
+    published: 1995
 };
+
 /* const { any, arrayContaining } = expect;
 const book = expect.objectContaining({
     id: any(Number),
@@ -20,25 +19,31 @@ const book = expect.objectContaining({
 });
 length > 0 ? arrayContaining([book]) : arrayContaining([]) */
 
-describe("Testing the getAllBooks method that gets (if any) all books", () => {
-    it("Should return a success answer with or without all books", async () => {
-        const res = await request(app).get("/books");
-        expect(res.status).toBe(200);
-    });
+test("Should return all books", async () => {
+    const res = await request(app).get("/books");
+
+    expect(res.status).toBe(200);
 });
 
-describe("Testing the createBook method that creates a book", () => {
-    it("It should be possible to create a book", async () => {
+describe("Testing 'createBook'", () => {
+    it("Should add a book", async () => {
         const res = await request(app)
             .post("/books")
-            .send(generateBook());
+            .send(book);
 
         expect(res.status).toBe(201);
     });
-});
 
-describe("", () => {
-    it("", () => {
+    it("Should not add a book", async () => {
+        const res = await request(app)
+            .post("/books")
+            .send({
+                title: "book 1",
+                author: "",
+                pages: 300,
+                published: 1995
+            });
 
+        expect(res.status).toBe(400);
     });
 });
