6dda640fadd7e3324e4e22418d5b4731dc16d7f0
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-02-01 15:20:22 -0300

    refactor: Improvements

--- tests/validateJSON.test.js
+++ tests/validateJSON.test.js
@@ -1,25 +1,17 @@
-const express = require("express");
-const request = require("supertest");
 const validateJSON = require("../source/middlewares/validateJSON");
 
-const app = express();
-
-beforeAll(() => {
-    app.get("/", validateJSON, (req, res) => res.send("Ok"));
-    app.listen(3002);
-});
-
 test("Testing 'validateJSON'", async () => {
-    const book = {
-        title: "book 1",
-        author: "author 1",
-        published: 1995,
-        pages: 300
+    const next = () => { };
+    const res = {};
+    const req = {
+        body: book = {
+            title: "",
+            author: "",
+            published: 0,
+            pages: 0
+        }
     };
 
-    const res = await request(app).get("/").send(book);
-
-    console.log(res.body);
-
-    //expect(() => { validateJSON({ body: book }) }).toThrow();
+    expect(() => validateJSON(req, res, next))
+        .toThrow();
 });
