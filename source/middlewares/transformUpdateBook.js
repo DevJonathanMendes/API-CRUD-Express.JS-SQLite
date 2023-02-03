ac43530f817f05aee9392c0eb3066c272f9a0ee7
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-02-03 04:22:50 -0300

    refactor: Improvements code

--- source/middlewares/transformUpdateBook.js
+++ source/middlewares/transformUpdateBook.js
@@ -1,9 +1,12 @@
+const normalize = value =>
+    typeof value == "string" ? value.toLowerCase() : value;
+
 const transformBook = (req, res, next) => {
     try {
         const entries = Object.entries(req.body);
 
-        const transformedEntries = entries.map(([key, value]) =>
-            `${key}='${typeof value == "string" ? value.toLowerCase() : value}'`);
+        const transformedEntries = entries
+            .map(([key, value]) => `${key}='${normalize(value)}'`);
 
         const transformedBook = transformedEntries.toString();
 
