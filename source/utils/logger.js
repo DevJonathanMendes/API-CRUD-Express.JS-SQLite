d89e46cf1d981831a27624c3d33fa55ee5ddbf47
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-03-29 17:16:54 -0300

    Improvements

--- source/utils/logger.js
+++ source/utils/logger.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const { createLogger, transports, format } = require("winston");
 const { combine, timestamp, printf } = format;
 
-const level = () => {
-    const env = process.env.NODE_ENV || "development"
-    return env === "development" ? "debug" : "warn"
-};
+const env = process.env.NODE_ENV || "development";
+const level = () =>
+    env === "development" ? "debug" : "warn";
 
 const myFormat = combine(
     timestamp({ format: "DD-MM-YYYY HH:mm:ss:ms" }),
