618f53c99fc72a641bbd355362f21a4452d53a66
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-04-01 16:32:03 -0300

    Improvements

--- source/models/ModelDatabase.js
+++ source/models/ModelDatabase.js
@@ -1,52 +1,14 @@
 const ServiceDatabase = require("../services/ServiceDatabase");
-
-const formatCreateSQL = obj => {
-    const format = (acc, value, index, array) =>
-        array.length - 1 == index
-            ? acc += `'${value}'`
-            : acc += `'${value}',`;
-
-    const getKeysObj = obj => Object.keys(obj).toString();
-    const getValuesObj = obj => Object.values(obj).reduce(format, "");
-
-    const columns = getKeysObj(obj);
-    const values = getValuesObj(obj);
-
-    return { columns, values };
-};
-
-const formatTableSQL = rows => {
-    const concat = (str1, str2) => str1 + str2.join(" ");
-    const format = (acc, values, index, array) =>
-        array.length - 1 == index
-            ? concat(acc, values)
-            : `${concat(acc, values)},`;
-
-    return Object.entries(rows).reduce(format, "");
-};
-
-const formatUpdateSQL = obj => {
-    const normalize = value =>
-        typeof value == "string" ? value.toLowerCase() : value;
-
-    const entries = Object.entries(obj);
-
-    const transformedEntries = entries
-        .map(([key, value]) => `${key}='${normalize(value)}'`);
-
-    const transformedObj = transformedEntries.toString();
-
-    return transformedObj;
-};
+const formatSQL = require("../utils/formatSQL");
 
 class ModelDatabase {
-    constructor(tableName, rows) {
+    constructor(tableName, columns) {
         this.tableName = tableName;
-        this.db = new ServiceDatabase(tableName, formatTableSQL(rows));
+        this.db = new ServiceDatabase(tableName, formatSQL.table(columns));
     };
 
     create(obj) {
-        return this.db.create(formatCreateSQL(obj));
+        return this.db.create(formatSQL.create(obj));
     };
 
     read(id) {
@@ -56,7 +18,7 @@ class ModelDatabase {
     update(id, obj) {
         return this.db.read(id)
             .then(found => found
-                ? this.db.update(id, formatUpdateSQL(obj))
+                ? this.db.update(id, formatSQL.update(obj))
                 : "Not Found"
             )
             .catch(err => { throw err });
